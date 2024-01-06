fbc8968f2e3ea1d762b5a294d51178f44584502d
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-06 19:56:12 +0530

    project-descrption-updated

--- src/app/projects/projectList.js
+++ src/app/projects/projectList.js
@@ -1,11 +1,11 @@
-import { hero, todolist, calorie, tictactoe, quiz,swaad,neurotrain,querysolver,recipemaker,quizapp} from '../images/index'
+import {swaad,neurotrain,querysolver,recipemaker,quizapp} from '../images/index'
 import { portfoliopic } from '../images/index';
 const list = [
     {
         id: 1,
         title: "NeuroTrain",
         link: 'https://lessonifyy.vercel.app/',
-        about: "Welcome to your ultimate health companion! Our website not only tracks your calories but transforms your diet into a personalized wellness experience. Enjoy real-time insights, receive motivational email updates, and explore intuitive tools for effortless fitness. Redefine your relationship with food, embrace a healthier lifestyle, and let us guide you on your journey to a happier you!",
+        about: "This application is primarily used to identify cognitive disabilities in people of any age. It does this by taking a screening test and estimating a user's likelihood of having a disability such as dyslexia or dyscalculia. It also offers various ways to monitor a patient's progress and create a graph as a result.",
         technologies: ['MERN', '', '', 'tailwind', '', '', 'bcrypt'],
         image: neurotrain,
     },
@@ -13,7 +13,7 @@ const list = [
         id:2,
         title:'Food HUB',
         link:'',
-        about:'This is a webiste for a cloud kitchen at kolkata, where you can order food online,you can find about different dishes and also you can contact them through this website.',
+        about:'Built a cross-platform React Native app using Expo CLI, featuring restaurant favoriting, local storage, GoogleMaps integration for restaurant display, Firebase for user authentication, and added animations to the home screen.',
         technologies: ['React Native', 'Expo CLI', 'Firebase', 'CSS'],
         image:swaad,
     },
@@ -28,21 +28,21 @@ const list = [
         id: 4,
         title: 'Query Solver',
         link: 'https://drive.google.com/file/d/1hgqggkTCGiok0oQ7q-s1cZjkixJUXHyR/view?usp=sharing',
-        about: 'Play the classic Tic Tac Toe game with your friends and family. Enjoy the game with a beautiful UI and a smooth experience.',
+        about: 'Essentially, query solver is a client-administrator platform where clients can ask questions if they have any, and users from the organization can respond to those requests by means of live chat. ',
         technologies: ['React', 'Web-Sockte','Tailwind','Nodejs','Chakra-UI'],
         image: querysolver,
     }, {
         id: 5,
         title: 'Recipe Maker',
         link: 'https://drive.google.com/file/d/1n5OV5bqzFsxOhwPekbdb9rlUmdUDiJIo/view',
-        about: 'Create your own to-do list and manage your tasks. Add, delete, and update your tasks with a beautiful UI.',
+        about: 'Recipe Maker is an application that allows users to share recipes by uploading pictures, details about the preparation procedure, and an estimate of the ingredients needed. It also has features that allow users to edit, remove, and like/comment on their own posts as well as see other users recipes that are posted on the platform.',
         technologies: ['Reactjs', 'MERN', 'Firebase', 'bcrypt'],
         image: recipemaker,
     }, {
         id: 6,
         title: "Quiz App",
         link: 'https://quiz-aiy81ca09-001priyanshu.vercel.app/',
-        about: "Elevate your DSA expertise with our quiz app! Quickly test your Skills. Receive instant feedback on your performance and uncover correct answers at the quiz's conclusion. Assess and enhance your understanding of DSA effortlessly!",
+        about: "This program functions as a platform similar to the well-known game show KBC, where users are given questions and prizes for answering correctly. If they answer a question incorrectly, the total money earned thus far will be earned and game will over, and if they answer correctly, they move on to the next one. ",
         technologies: ['MERN', 'tailwind','redux'],
         image: quizapp,
     }
